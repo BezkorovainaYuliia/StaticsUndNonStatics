77cf17fc340078c69b6818a12f984652914407f0
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-03 20:11:57 +0200

    Fertig mit dem Thema Static/NonStatic

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(1,2));
+        System.out.println(Calculator.sub(2,3));
+        System.out.println(Calculator.mul(3,4) );
+        System.out.println(Calculator.div(5,3));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+    public static double div(int a, int b) {
+        return (double )a / b;
+    }
+}
